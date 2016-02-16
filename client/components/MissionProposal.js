26e9418aea0f651b09354ac7c2db31aaf2282e56
Hack Reactor Students <accounts@hackreactor.com>
2016-02-15 20:53:00 -0800

    Add .gitignore, .bowerrc, and create node/express server in server.js  Changes to be committed: 	new file:   .bowerrc 	new file:   .gitignore 	modified:   client/components/MissionProposal.js 	modified:   client/index.html 	modified:   server/server.js

--- client/components/MissionProposal.js
+++ client/components/MissionProposal.js
@@ -1,3 +1,3 @@
 // Child of Board.js
 // Has state: {proposalCounter} and function to set counter
-// Passed players and set mission state function for Board.js
+// Passed players and set missions state function for Board.js
--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
+var express = require('express');
+var app = express();
 
+// configure our server with all the middleware and routing
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
+// start listening to requests on port 8000
+app.listen(8000);
+
+// export our app for testing and flexibility, required by index.js
+module.exports = app;
