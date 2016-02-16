aa804dbc95b49859e990f57636c81e1602e81c26
Hack Reactor Students <accounts@hackreactor.com>
2016-02-15 22:44:16 -0800

    Add outline for Board.js view 	modified:   client/components/Board.js

--- client/components/Board.js
+++ client/components/Board.js
@@ -6,3 +6,49 @@
 // Renders Missions on top 
 // Renders MissionProposal || MissionPhase || Assasination || GameOver on bottom
 // Renders MissionStats on side (advanced)
+
+class Board extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      Players: props.players,
+      missions: props.missions,
+      currentMission: 0,
+      victor: undefined 
+    }
+  }
+
+  set(num) {
+    this.setState({
+      numOfPlayers: num
+    })
+  }
+
+  setVictor(victor) {
+    this.setState({
+      victor: victor
+    })
+  }
+
+  incrementMission() {
+    this.setState({
+      roles: roles
+    })
+  }
+
+  render(){
+    if (!this.state.roles) {
+      return <div>
+                <Init setPlayers={this.setNumOfPlayers.bind(this)}
+                  setNames={this.setNames.bind(this)}
+                  setRoles={this.setRoles.bind(this)}/>
+              </div>
+    } else { 
+      var players = generatePlayers(this.state.names, this.state.roles); //Returns object with name:role pairs
+      return <div>
+                <Board players={players}/>
+              </div>
+    } 
+  }
+}
